9ac2dca1a69e78fa1c66be586385b91c404a833d
Mary-Frye <deimospho@gmail.com>
2022-05-22 10:04:22 -0400

    clen up

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+window.prompt(weeklyExpenseQuestions[0])
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+console.log(answer)
+
+for(let i=0; i < weeklyExpensesQuestions.length; i++){
+    let answer= parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+
